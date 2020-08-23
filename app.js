a69d62afdc13b1c10e3c90048155bb6a7cec172d
Steve Larsen <slarsen-3@att.net>
2020-08-23 17:37:38 -0500

    HTTPS

--- app.js
+++ app.js
@@ -28,6 +28,20 @@ connect.then(
 
 var app = express();
 
+app.all("*", (req, res, next) => {
+  if (req.secure) {
+    return next();
+  } else {
+    console.log(
+      `Redirecting to: https://${req.hostname}:${app.get("secPort")}${req.url}`
+    );
+    res.redirect(
+      301,
+      `https://${req.hostname}:${app.get("secPort")}${req.url}`
+    );
+  }
+});
+
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
@@ -36,7 +50,6 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 //app.use(cookieParser("12345-67890-09876-54321"));
-
 
 app.use(passport.initialize());
 
