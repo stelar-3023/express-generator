5ecbf0bbb609ee64fa68cbfc584e16e60a860029
Steve Larsen <slarsen-3@att.net>
2020-08-27 13:44:06 -0500

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   secretKey: "12345-67890-09876-54321",
   mongoUrl: "mongodb://localhost:27017/nucampsite",
+  facebook: {
+    clientId: "310995716673788",
+    clientSecret: "fe1fe79028b7ea2fc017e4fee6a99835",
+  },
 };
--- models/user.js
+++ models/user.js
@@ -11,6 +11,7 @@ const UserSchema = new Schema({
     type: String,
     default: "",
   },
+  facebookId: String,
   admin: {
     type: Boolean,
     default: false,
